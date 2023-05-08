dbcd9f075ee13a5b17587dd83872a050d791a536
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-08 18:49:34 -0300

    Feat: Requisito 22 Concluído

--- src/components/ButtonCategorie.jsx
+++ src/components/ButtonCategorie.jsx
@@ -1,14 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import SearchBarContext from '../context/SearchBarContext';
 
 export default function ButtonCategorie({ name }) {
-  const { fetchByCategory } = useContext(SearchBarContext);
+  const { fetchByCategory, searchBtn } = useContext(SearchBarContext);
+  const [buttonSelected, setButtonSelected] = useState(false);
 
   return (
     <button
       data-testid={ `${name}-category-filter` }
-      onClick={ () => fetchByCategory(name) }
+      onClick={ () => (
+        buttonSelected ? (searchBtn('', 'Name'), setButtonSelected(!buttonSelected))
+          : (fetchByCategory(name), setButtonSelected(!buttonSelected))) }
     >
       {name}
     </button>
